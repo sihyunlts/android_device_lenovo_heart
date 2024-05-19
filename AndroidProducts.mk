426c20b107d67ae56deff89654153ce6c364b7cc
Michael Bestas <mkbestas@lineageos.org>
2024-05-20 01:45:16 +0300

    heart: Drop COMMON_LUNCH_CHOICES
    As of 14 QPR2 this is broken, let's remove it instead of updating it
    every 3 months. breakfast is the way to go.
    
    Change-Id: I097bd54b894d9a49c7ce8b1ab6f343349e268fd8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,8 @@
 #
-# Copyright (C) 2019-2021 The LineageOS Project
+# Copyright (C) 2019-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_heart.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_heart-user \
-    lineage_heart-userdebug \
-    lineage_heart-eng
